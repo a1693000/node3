735d5de6dabc022fe7422c5e92a089ee65796d0f
Erick Zen Iwamoto <erick_zen_@hotmail.com>
2019-03-15 09:38:47 -0300

    ultimo commit da aula

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,9 @@
 module.exports = {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '2c63ec3671ce3a',
-    pass: '0e148a007f7954'
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD
   }
 }
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 module.exports = {
-  dsn: 'https://2e511c14c7314fb6ab8d73673cdfe125@sentry.io/1415833'
+  dsn: process.env.SENTRY_DSN
 }
